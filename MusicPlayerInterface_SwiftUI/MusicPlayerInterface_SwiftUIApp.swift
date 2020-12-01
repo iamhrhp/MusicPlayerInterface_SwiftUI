d00948660a1fffc91353d83545ef90b9ce1bc084
Parvej khan <51136789+iamhrhp@users.noreply.github.com>
2020-12-01 04:34:48 -0800

    Initial Commit

---
+++ MusicPlayerInterface_SwiftUI/MusicPlayerInterface_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerInterface_SwiftUIApp.swift
+//  MusicPlayerInterface_SwiftUI
+//
+//  Created by HRHP Parvej on 01/12/20.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerInterface_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
